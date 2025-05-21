8792c6eeb1f0323aa12806dce3064fb580cafbef
Kerubel Aklilu <aklilu.kerubel@gmail.com>
2025-05-20 23:26:29 -0400

    Added lab2e.py

---
+++ lab2e.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+#Author: Kerubel Aklilu
+#Author ID: 111658217
+#Date Created: 2025/05/15
+
+timer = 10
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
