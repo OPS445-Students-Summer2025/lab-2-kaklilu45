a8c7a9aedaf83b23283bca0ed36e6abcc09d044c
Kerubel Aklilu <aklilu.kerubel@gmail.com>
2025-05-20 23:26:52 -0400

    Added lab2f.py

---
+++ lab2f.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+#Author: Kerubel Aklilu
+#Author ID: 111658217
+#Date Created: 2025/05/15
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
+
